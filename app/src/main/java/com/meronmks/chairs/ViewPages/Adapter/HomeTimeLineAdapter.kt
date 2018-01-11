66cccf2cb784516dd0c50e17d8f8820cc4bc2725
meronmks.8914@gmail.com <meronmks.8914@gmail.com>
2018-01-12 00:18:00 +0900

    とりあえずカスタム絵文字表示を作成（大きさがおかしいのはご愛敬）

--- app/src/main/java/com/meronmks/chairs/ViewPages/Adapter/HomeTimeLineAdapter.kt
+++ app/src/main/java/com/meronmks/chairs/ViewPages/Adapter/HomeTimeLineAdapter.kt
@@ -1,17 +1,17 @@
 package com.meronmks.chairs.ViewPages.Adapter
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.meronmks.chairs.R
 import com.meronmks.chairs.data.model.TimeLineStatus
 import kotlinx.android.synthetic.main.toot_item.view.*
+import android.webkit.WebSettings
+import com.meronmks.chairs.extensions.fromHtml
+
 
 /**
  * Created by meron on 2018/01/09.
@@ -26,9 +26,9 @@ class HomeTimeLineAdapter(context: Context) : ArrayAdapter<TimeLineStatus>(conte
             view = layoutInflater.inflate(R.layout.toot_item, parent, false)!!
         }
         val item = getItem(position) as TimeLineStatus
-
+        view.displayNameTextView.text = item.displayName
         view.userNameTextView.text = item.userName
-        view.tootTextView.text = item.content(context)
+        view.tootTextView.text = item.content().fromHtml(context, view.tootTextView)
         Glide.with(context).load(item.avater).into(view.avatarImageButton)
         return view
     }
