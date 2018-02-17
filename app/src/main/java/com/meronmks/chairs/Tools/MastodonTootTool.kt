f44f2c8eeba1bf1f80894bd663a7ce2b8f9a6cbb
meronmks <meronmks.8914@gmail.com>
2018-02-17 23:53:44 +0900

    雑にトゥートの実装

---
+++ app/src/main/java/com/meronmks/chairs/Tools/MastodonTootTool.kt
@@ -0,0 +1,23 @@
+package com.meronmks.chairs.Tools
+
+import com.google.gson.Gson
+import com.sys1yagi.mastodon4j.MastodonClient
+import com.sys1yagi.mastodon4j.api.entity.Status
+import com.sys1yagi.mastodon4j.api.method.Statuses
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import okhttp3.OkHttpClient
+
+/**
+ * Created by meron on 2018/02/17.
+ */
+
+class MastodonTootTool(private val instanceName : String, private val accessToken : String){
+    fun tootAsync(tootText : String, inReplyToId : Long?, mediaIds: List<Long>?, sensitive : Boolean, spoilerText: String?, visibility: Status.Visibility) = async(CommonPool){
+        val client: MastodonClient = MastodonClient.Builder(instanceName, OkHttpClient.Builder(), Gson())
+                .accessToken(accessToken)
+                .build()
+        val statuses = Statuses(client)
+        return@async statuses.postStatus(tootText, inReplyToId, mediaIds, sensitive, spoilerText, visibility).execute()
+    }
+}
--- app/src/main/java/com/meronmks/chairs/ViewPages/HomeViewPage.kt
+++ app/src/main/java/com/meronmks/chairs/ViewPages/HomeViewPage.kt
@@ -3,14 +3,24 @@ package com.meronmks.chairs.ViewPages
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.meronmks.chairs.R
+import com.meronmks.chairs.Tools.DataBaseTool
+import com.meronmks.chairs.Tools.MastodonTootTool
 import com.meronmks.chairs.ViewPages.Adapter.HomeFragmentPagerAdapter
+import com.meronmks.chairs.extensions.showToastandLogE
+import com.sys1yagi.mastodon4j.api.entity.Status
 import kotlinx.android.synthetic.main.activity_home_view_page.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 
 class HomeViewPage : AppCompatActivity() {
 
+    lateinit var dataBase : DataBaseTool
+    lateinit var tootTool : MastodonTootTool
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_view_page)
+        dataBase = DataBaseTool(baseContext)
+        tootTool = MastodonTootTool(dataBase.readInstanceName(), dataBase.readAccessToken())
         homeViewPager.adapter = HomeFragmentPagerAdapter(supportFragmentManager)
         homeTabs.setupWithViewPager(homeViewPager)
         homeTabs.getTabAt(0)?.setIcon(R.drawable.ic_home_black_24dp)
@@ -18,5 +28,21 @@ class HomeViewPage : AppCompatActivity() {
         homeTabs.getTabAt(2)?.setIcon(R.drawable.ic_format_list_bulleted_black_24dp)
         homeTabs.getTabAt(3)?.setIcon(R.drawable.ic_people_black_24dp)
         homeTabs.getTabAt(4)?.setIcon(R.drawable.ic_public_black_24dp)
+        sendTootImageButton.setOnClickListener {
+            launch(UI) {
+                try {
+                    tootTool.tootAsync(tootEditText.text.toString(), null, null, false, null, Status.Visibility.Public).await()
+                    tootEditText.text.clear()
+                }catch (e: Exception){
+                    e.message?.showToastandLogE(baseContext)
+                }
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        dataBase.close()
     }
 }
