f0df8cd011b89ab06b49631c7b2d2d536f6a2c4b
meronmks <meronmks.8914@gmail.com>
2018-04-15 23:37:54 +0900

    通知画面でのタッチ判定を調整

--- app/src/main/java/com/meronmks/chairs/ViewPages/Adapter/RecyclerView/NotificationAdapter.kt
+++ app/src/main/java/com/meronmks/chairs/ViewPages/Adapter/RecyclerView/NotificationAdapter.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.meronmks.chairs.Annotation.GlideApp
 import com.meronmks.chairs.R
 import com.meronmks.chairs.ViewPages.ViewHolder.NotificationViewHolder
 import com.meronmks.chairs.ViewPages.ViewHolder.TimeLineViewHolder
@@ -46,8 +47,17 @@ class NotificationAdapter(private val context: Context, private val itemClickLis
             it.userNameTextView.text = "@${item.actionUserName}"
             it.tootTextView.text = item.content().fromHtml(context, it.tootTextView)
             it.timeTextView.text = item.createAt(context, java.lang.System.currentTimeMillis())
-            Glide.with(context).load(item.actionAvater).into(it.avatarImageButton)
+            GlideApp.with(context).load(item.actionAvater).into(it.avatarImageButton)
             //タッチイベントの処理
+            it.displayNameTextView.setOnTouchListener { v:View, event: MotionEvent ->
+                val textView : TextView = v as TextView
+                val m = MutableLinkMovementMethod()
+                textView.movementMethod = m
+                val mt = m.onTouchEvent(textView, textView.text as Spannable, event)
+                textView.movementMethod = null
+                textView.isFocusable = false
+                return@setOnTouchListener mt
+            }
             it.tootTextView.setOnTouchListener { v:View, event: MotionEvent ->
                 val textView : TextView = v as TextView
                 val m = MutableLinkMovementMethod()
