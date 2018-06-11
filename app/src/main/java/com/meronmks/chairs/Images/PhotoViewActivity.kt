3532c7f2f789b2b2b544ae1c6d1669ef89b280f8
meronmks <meronmks.8914@gmail.com>
2018-06-11 22:13:22 +0900

    画像の拡大表示画面からバーを消した

--- app/src/main/java/com/meronmks/chairs/Images/PhotoViewActivity.kt
+++ app/src/main/java/com/meronmks/chairs/Images/PhotoViewActivity.kt
@@ -13,6 +13,7 @@ class PhotoViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_view)
+        supportActionBar?.hide()
         val url = intent.getStringExtra("url")
         val options = RequestOptions()
                 .placeholder(R.drawable.ic_autorenew_black_24dp)
