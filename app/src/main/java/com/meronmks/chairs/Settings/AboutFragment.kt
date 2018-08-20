f1e5b6de016c0c0c5978e45d3513936a753507d5
meronmks <meronmks.8914@gmail.com>
2018-08-20 21:31:32 +0900

    設定画面のAboutを少し作成

--- app/src/main/java/com/meronmks/chairs/Settings/AboutFragment.kt
+++ app/src/main/java/com/meronmks/chairs/Settings/AboutFragment.kt
@@ -2,9 +2,14 @@ package com.meronmks.chairs.Settings
 
 import android.os.Bundle
 import android.preference.PreferenceFragment
+import com.meronmks.chairs.BuildConfig
+import com.meronmks.chairs.R
 
 class AboutFragment : PreferenceFragment(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        addPreferencesFromResource(R.xml.about_fragment)
+        findPreference("appVersionText").title = "%s Ver%s".format(resources.getString(R.string.app_name), BuildConfig.VERSION_NAME)
+        findPreference("appVersionText").summary = "作者:meronmks\r\nPowered by mastodon4j"
     }
 }
