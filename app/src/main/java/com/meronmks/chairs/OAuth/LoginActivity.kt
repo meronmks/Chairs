0df49c2eede9f2496224621c2beaf8f037d6af70
meronmks <meronmks.8914@gmail.com>
2018-11-05 22:34:20 +0900

    認証済みアプリ一覧が爆発炎上する現象の回避

--- app/src/main/java/com/meronmks/chairs/OAuth/LoginActivity.kt
+++ app/src/main/java/com/meronmks/chairs/OAuth/LoginActivity.kt
@@ -4,20 +4,23 @@ import android.annotation.SuppressLint
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 import com.meronmks.chairs.R
 import com.meronmks.chairs.Tools.Database.AccountDataBaseTool
 import com.meronmks.chairs.Tools.MastodonAccountTool
 import com.meronmks.chairs.Tools.MastodonLoginTool
+import com.meronmks.chairs.extensions.showToastLogE
 import com.meronmks.chairs.initialize.MainActivity
 import com.sys1yagi.mastodon4j.api.entity.auth.AppRegistration
+import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
-import com.meronmks.chairs.extensions.showToastLogE
-import com.sys1yagi.mastodon4j.api.exception.Mastodon4jRequestException
+import kotlinx.coroutines.experimental.tasks.await
 
 
 class LoginActivity : AppCompatActivity() {
@@ -27,19 +30,34 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var userName : String
     private lateinit var appRegistration : AppRegistration
     private lateinit var accessToken : String
+    private lateinit var instanceName : String
+    private var clientId : String = ""
+    private var clientSecret : String = ""
+    private val fireBaseDB : FirebaseFirestore = FirebaseFirestore.getInstance()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         LoginButton.isEnabled = false
-        mastodonLogin = MastodonLoginTool(intent.getStringExtra("instanceName"))
-        step1TextView.text = "${intent.getStringExtra("instanceName")}${getString(R.string.step1Text)}"
+        instanceName = intent.getStringExtra("instanceName")
+        mastodonLogin = MastodonLoginTool(instanceName)
+        step1TextView.text = "$instanceName${getString(R.string.step1Text)}"
     }
 
     fun startOAuth(view: View) = launch(UI){
-        appRegistration = mastodonLogin.registerAppAsync().await()
-        val url = mastodonLogin.oAuthAsync(appRegistration.clientId).await()
+        val document = getMInstanceLists() ?: return@launch
+        if(document.exists()) {
+            clientId = document.data?.get("clientId").toString()
+            clientSecret = document.data?.get("clientSecret").toString()
+        }else{
+            appRegistration = mastodonLogin.registerAppAsync().await()
+            clientId = appRegistration.clientId
+            clientSecret = appRegistration.clientSecret
+            val instanceData: HashMap<String, Any> = hashMapOf("clientId" to clientId, "clientSecret" to clientSecret)
+            addMInstanceLists(instanceData)
+        }
+        val url = mastodonLogin.oAuthAsync(clientId).await()
         val intent = Intent(Intent.ACTION_VIEW, url)
         LoginButton.isEnabled = true
         startActivity(intent)
@@ -50,12 +68,12 @@ class LoginActivity : AppCompatActivity() {
         val cm = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val cd = cm.primaryClip
         try{
-            accessToken = mastodonLogin.loginAsync(appRegistration.clientId, appRegistration.clientSecret, cd.getItemAt(0).text.toString()).await().accessToken
-            val account = MastodonAccountTool(appRegistration.instanceName, accessToken)
+            accessToken = mastodonLogin.loginAsync(clientId, clientSecret, cd.getItemAt(0).text.toString()).await().accessToken
+            val account = MastodonAccountTool(instanceName, accessToken)
             val user = account.getVerifyCredentialsAsync().await()
             if(user.userName.isEmpty()) return@launch
             userName = user.userName
-            accountDataBase.saveAccount(appRegistration.instanceName, userName, accessToken)
+            accountDataBase.saveAccount(instanceName, userName, accessToken)
             val intent = Intent(baseContext, MainActivity::class.java)
             startActivity(intent)
             finish()
@@ -67,5 +85,18 @@ class LoginActivity : AppCompatActivity() {
         catch (e: Exception){
             e.message.toString().showToastLogE(baseContext)
         }
+    }
+
+    private suspend fun getMInstanceLists(): DocumentSnapshot? {
+        return fireBaseDB.collection("MInstanceLists")
+                .document(instanceName)
+                .get()
+                .await()
+    }
+
+    private fun addMInstanceLists(instanceData: HashMap<String, Any>){
+        fireBaseDB.collection("MInstanceLists")
+                .document(instanceName)
+                .set(instanceData as Map<String, Any>)
     }
 }
