7c64baf58c29e74ef4fa4c6abacda369679fb1e6
meronmks <meronmks.8914@gmail.com>
2019-04-29 00:37:36 +0900

    fix typo

--- app/src/main/java/com/meronmks/chairs/ViewPages/ViewHolders/NotificationViewHolder.kt
+++ app/src/main/java/com/meronmks/chairs/ViewPages/ViewHolders/NotificationViewHolder.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.toot_item.view.*
  * Created by meron on 2018/03/10.
  */
 class NotificationViewHolder(itemView: View) : ViewHolder(itemView) {
-    val lastItemMaginSpace = itemView.lastItemMaginSpace!!!!
+    val lastItemMaginSpace = itemView.lastItemMaginSpace!!
     val displayNameTextView = itemView.displayNameTextView!!
     val userNameTextView = itemView.userNameTextView!!
     val tootTextView = itemView.tootTextView!!
